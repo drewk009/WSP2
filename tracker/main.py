2eabee875e679846c6a552eb0e052aae4a343e96
root <root@x200s.(none)>
2011-11-14 20:49:38 -0500

    timestamp working

--- tracker/main.py
+++ tracker/main.py
@@ -1,4 +1,12 @@
+from datetime import date, datetime, timedelta
+import time
 import twitter
 client = twitter.Api()
-latest_posts = client.GetUserTimeline("we_are_thankful")
-print [s.text for s in latest_posts]
+#latest_posts = client.GetUserTimeline("we_are_thankful")
+time_stamp = date.fromtimestamp(time.time()).strftime("%A %d. %B %Y")
+time_stamp = time_stamp
+#print [s.text for s in latest_posts]
+print time_stamp
+fmt = '%H:%M:%S'
+time = datetime.now().strftime(fmt)
+print time
